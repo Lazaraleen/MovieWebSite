d577a29b0c065134b020e3c174b925c04aa304ce
Myriam JAYLE <myriamjayle@gmail.com>
2024-01-27 15:37:46 +0100

    Début du site

---
+++ src/components/MovieContent.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './movieContent.css';
+import titleImg from '../images/transformer-title.png';
+
+function MovieContent() {
+    return (
+        <div className="content active">
+            <img src={titleImg} alt="Movie Title" className="movie-title" />
+            <h4><span>Year</span>
+                <span><i>age</i></span>
+                <span>length</span>
+                <span>category</span>
+            </h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sodales libero sit amet volutpat tincidunt. Morbi cursus blandit turpis non aliquam. In vel erat sit amet velit pulvinar ullamcorper. Ut quis luctus nibh. Donec rhoncus enim id purus rhoncus condimentum eget vitae turpis. Praesent eu massa consectetur, malesuada est ac, porttitor neque. Donec lacinia orci quis risus tincidunt, ut dapibus sem tristique. Praesent lacinia vestibulum orci ut tempor. Mauris nec luctus lectus, non accumsan magna.</p>
+            <div className="button">Button</div>
+        </div>
+    )
+}
+
+export default MovieContent;
---
+++ src/data/navListData.js
@@ -0,0 +1,22 @@
+const navListData = [
+    {
+        _id: 1,
+        link: '#',
+        name: 'home',
+        active: true,
+    },
+    {
+        _id: 2,
+        link: '#schedule',
+        name: 'schedule',
+        active: false,
+    },
+    {
+        _id: 3,
+        link: '#trend',
+        name: 'trend',
+        active: false,
+    },
+];
+
+export default navListData;
